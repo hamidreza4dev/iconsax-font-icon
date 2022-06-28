28590ae014da4f6d3744e4de90bfe55c6a12a94d
Hamidreza Firouzi <hamdireza4dev@gmail.com>
2022-06-28 19:27:29 +0430

    add html sample

--- fantasticonrc.js
+++ fantasticonrc.js
@@ -2,7 +2,7 @@ module.exports = {
   inputDir: './src/icons',
   outputDir: './dist/fonts',
   fontTypes: ['ttf', 'woff', 'woff2', 'eot'],
-  assetTypes: ['css'],
+  assetTypes: ['css', 'html'],
   fontsUrl: './fonts',
   pathOptions: {
     css: './dist/icons.css',
